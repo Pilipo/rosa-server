b53da2d3eb6d9d51daae871f31290dfca81eb2e9
Phillip <pl@philliplehner.com>
2020-08-14 10:30:41 -0500

    Auth testing

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('login/google', 'Auth\LoginController@redirectToProvider');
-// Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback');
+Route::get('login/google', 'Auth\LoginController@redirectToProvider');
+Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback');
 // Route::get('login/{provider}', 'SocialController@redirect');
 // Route::get('login/{provider}/callback','SocialController@Callback');
 
