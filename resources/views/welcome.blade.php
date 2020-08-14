382826d692d9a7751302e2d70ad023caddb453ff
Phillip <pl@philliplehner.com>
2020-08-14 10:45:35 -0500

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rosa
                 </div>
 
                 <div class="links">
