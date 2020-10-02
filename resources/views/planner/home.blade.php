db5c0a287ff7234c8cf1c29d733a3dc74529a08e
Pilipo <pl@philliplehner.com>
2020-10-02 19:45:36 +0000

    Cleans up test interface

--- resources/views/planner/home.blade.php
+++ resources/views/planner/home.blade.php
@@ -7,28 +7,36 @@
             <div class="card">
                 <div class="card-header center">{{ $time->format('D, M d') }} to {{ $time->endOfWeek()->format('D, M d') }}</div>
                 <div class="card-body">
-                    <div class="row text-center">
+                    <div class="row">
                         @for ($i = 0; $i < 7; $i++)
-                        <div class="col-6">
+                        <div class="col-lg-6 col-sm-12">
                             <div class="card mb-2">
                                 <div class="card-header">
                                     {{ $time->add($i, 'day')->format('D d') }}
                                 </div>
-                                <div class="card-body">
-                                    
-                                    <ul>
+                                <!-- <div class="card-body">   -->
+                                    <ul class="list-group list-group-flush">
                                     @foreach ($recipes as $recipe)
                                         @foreach ($recipe->dates as $date)
                                             @if ($time->add($i, 'day')->diffInDays($date->meal_day) == 0)
-                                            <li>
-                                                    {{ $recipe->name }} - 
-                                                    {{ $date->meal_day }}
-                                            </li>
+
+                                        <li class="list-group-item clearfix">
+                                            {{ $recipe->name }}
+                                            <span class="float-right">
+                                                <span class="btn btn-xs btn-default border">remove</span>
+                                            </span>
+                                        </li>
                                             @endif
                                         @endForeach
                                     @endForeach
+                                        <li class="list-group-item">
+                                            Add another...
+                                            <span class="float-right">
+                                                <span class="btn btn-xs btn-default border">add</span>
+                                            </span>
+                                        </li>
                                     </ul>
-                                </div>
+                                <!-- </div> -->
                             </div>
                         </div>
                         @endfor
