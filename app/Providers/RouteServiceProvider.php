48922d66b017015f0977bddb54ff897150f51aae
Phillip <pl@philliplehner.com>
2020-10-05 16:44:01 -0500

    Corrections for prod/dev detection

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -58,7 +58,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::domain('rosa.philliplehner.com')
+        Route::domain('rosa.philliplehner.' . ((env('APP_ENV') == 'production') ? 'com' : 'net'))
             ->middleware('web')
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
@@ -73,7 +73,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::domain('api.rosa.philliplehner.com')
+        Route::domain('api.rosa.philliplehner.' . ((env('APP_ENV') == 'production') ? 'com' : 'net'))
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
