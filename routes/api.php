588b809d2d83afd0da917a1f47d981fc476d9ef1
Phillip <pl@philliplehner.com>
2020-10-20 13:11:40 -0500

    adds version-based routes for API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::fallback(function() {
+    return response()->json([
+        'message' => 'Page Not Found.'
+    ], 404);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/', function () {
+    return ['hello'];
 });
