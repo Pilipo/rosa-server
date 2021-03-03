872f6421390b55282377c4cd65d2a2d310fe20e0
Phillip Lehner <pl@philliplehner.com>
2021-03-03 19:45:27 +0000

    Adds very basic seeder for recipes

---
+++ database/seeders/RecipeTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class RecipeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1, 100) as $idx) {
+            DB::table('recipes')->insert([
+                'name' => $faker->word(),
+                'servings' => rand(3, 48) . ' servings',
+                'user_id' => rand(0, 400)
+            ]);
+        }
+    }
+}
